0b1401cbf8dd34221bf909d258813350202a6e59
Rodrigo-Oak <132356249+Rodrigo-Oak@users.noreply.github.com>
2025-04-12 15:29:46 -0300

    primeiro commit
    um codigo simples para "começar"

---
+++ ETcommerce.cs
@@ -0,0 +1,13 @@
+
+ namespace ETcommerce.Models
+ {
+
+    public class Produto
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string Descricao { get; set; }
+        public string Imagem { get; set; }
+        public decimal Valor { get; set; }
+    }
+ }
